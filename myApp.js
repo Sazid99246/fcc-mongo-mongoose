652dd118818d4023b0c029da7edb5b8d28a01d8d
Sheikh MD Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2022-09-11 19:38:03 +0600

    created and save a record of a model

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://dbUser1:tSATRnSo21NpqmPB@cluster0.obsituy.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://dbUser1:tSATRnSo21NpqmPB@cluster0.obsituy.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true },(err)=>{
+  if(err){
+    console.log(err);
+  }
+  console.log("database connected");
+});
 
 const personSchema = mongoose.Schema({
   name: {
@@ -21,7 +26,15 @@ const personSchema = mongoose.Schema({
 let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  let person = new Person({
+    name: "Sazid",
+    age: 21,
+    favoriteFoods: ["sandwitch", "burger"]
+  });
+
+  person.save((err, data)=>{
+    done(null, data);
+  });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
